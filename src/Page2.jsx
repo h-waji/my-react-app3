7660d38dbbc5c6d776618dc9492986d6598bddb4
hamru <goo.hiro.w@gmail.com>
2024-03-10 16:27:50 +0900

    url parameter

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export const Page2 = () => {
     return (
         <div>
             <h2>Page2</h2>
+            <Link to="/page2/66">URL Parameter</Link>
         </div>
     );
 };
--- src/router/Page1Routes.jsx
+++ src/router/Page1Routes.jsx
@@ -1,6 +1,6 @@
-import { Page1 } from "../../Page1";
-import { Page1_A } from "../../Page1_A";
-import { Page1_B } from "../../Page1_B";
+import { Page1 } from "../Page1";
+import { Page1_A } from "../Page1_A";
+import { Page1_B } from "../Page1_B";
 
 export const page1Routes = [
     {
