361533bb270c5397f0b2106819d46601e26904ac
hamru <goo.hiro.w@gmail.com>
2024-03-10 17:03:07 +0900

    page transition with passing state

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
+    const arr = [...Array(66).keys()];
+    console.log(arr);
     return (
         <div>
             <h2>Page1</h2>
-            <Link to="/page1/A">1-A</Link>
+            <Link to={{ pathname: "/page1/A", state: arr }}>1-A</Link>
             <br />
             <Link to="/page1/B">1-B</Link>
         </div>
--- src/Page1_A.jsx
+++ src/Page1_A.jsx
@@ -1,4 +1,9 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1_A = () => {
+    const { state } = useLocation();
+    console.log(state);
+
     return (
         <div>
             <h3>Page1-Aだよ</h3>
